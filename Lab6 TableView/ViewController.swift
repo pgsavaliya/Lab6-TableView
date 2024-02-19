0721343400cec05d345759b714ce9bca9092cb46
Pgsavaliya <pgsavaliya01@gmail.com>
2024-02-18 21:10:01 -0500

    Initial Commit

---
+++ Lab6 TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab6 TableView
+//
+//  Created by Pavan savaliya on 2024-02-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
